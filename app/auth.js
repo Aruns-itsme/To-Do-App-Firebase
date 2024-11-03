1dd83b61f9690ce2cc94405e543657bd0aaefb55
Arun Saravanakumar <168937449+Aruns-itsme@users.noreply.github.com>
2024-11-02 20:23:25 -0400

    Update auth.js

--- app/auth.js
+++ app/auth.js
@@ -5,12 +5,13 @@ import { getFirestore, collection, addDoc, deleteDoc, updateDoc, onSnapshot, doc
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "", //Add api key
-  authDomain: "", //Add authDomain
-  projectId: "", Add projectId
-  storageBucket: "", Add storageBucket
-  messagingSenderId: "", Add messagingSenderId
-  appId: "" Add appId
+  apiKey: "AIzaSyDzwRzuL730q3AosTRnks0nojwNkHnSnKY", //Add api key
+  authDomain: "to-do-app-45049.firebaseapp.com", //Add authDomain
+  databaseURL: "https://to-do-app-45049-default-rtdb.firebaseio.com", //Add projectId
+  projectId: "to-do-app-45049", //Add storageBucket
+  storageBucket: "to-do-app-45049.firebasestorage.app", //Add messagingSenderId
+  messagingSenderId: "345173607030",
+  appId: "1:345173607030:web:ffcf8c030a4e0b018e5557" //Add appId
 };
 
 // Initialize Firebase
