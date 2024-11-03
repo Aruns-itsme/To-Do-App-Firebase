dfcfa33af81fb843267d5fbe0959910f343f1147
Arun Saravanakumar <168937449+Aruns-itsme@users.noreply.github.com>
2024-11-02 20:21:08 -0400

    Add files via upload

---
+++ helper/apiKeys.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzwRzuL730q3AosTRnks0nojwNkHnSnKY",
+  authDomain: "to-do-app-45049.firebaseapp.com",
+  databaseURL: "https://to-do-app-45049-default-rtdb.firebaseio.com",
+  projectId: "to-do-app-45049",
+  storageBucket: "to-do-app-45049.firebasestorage.app",
+  messagingSenderId: "345173607030",
+  appId: "1:345173607030:web:ffcf8c030a4e0b018e5557"
+};
+
+export default firebaseConfig;
